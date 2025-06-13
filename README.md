9d94106ef78f85c7a60d12102a3b4fbe4232fffe
MikaAmann <93271414+MikaAmann@users.noreply.github.com>
2025-06-13 18:04:27 +0200

    added more examples

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Seminararbeit
+
+| Test ID      | Category      | Topic |Expected Verdict | CPA Verdict | LLM Verdict | CPA Correct | LLM Correct | Error Type (CPA / LLM)|
+|--------------|---------------|-------|-----------------|-------------|-------------|-------------|-------------|-----------------------|
+| F-001   | Falsification || false    | true        | true        |   ❌          | ❌          | FN / FN   |
+| NO-001 | No Overflow   | Recursive Fibonnaci |true     | true     | true       | ✅          | ✅          | - / - |
+| NO-002 | No Overflow   | Even/Odd|false     | false     | false       | ✅          | ✅          | - / - |
+| NO-003 | No Overflow   | Ackermann function|true     | timeout     | true       | ❌           | ✅          | - / - |
+| NO-004 | No Overflow   | Recursive integer addition |false     | timeout     | false    | ❌           | ✅          | - / - |
+| NO-005 | No Overflow   | Array tiling |true     | true (unconfirmed)     | false       | (✅)          |  ❌         | - / FP|
+
+
+
+
+
+| test002 | termination   | | true     | timeout     | false       | ✅          | ❌          | FN / FN |
--- sv-comp_categories/NoOverflows/NO-004/NO-004.c
+++ sv-comp_categories/NoOverflows/NO-004/NO-004.c
@@ -1,6 +1,6 @@
 extern void abort(void);
 extern void __assert_fail(const char *, const char *, unsigned int, const char *) __attribute__ ((__nothrow__ , __leaf__)) __attribute__ ((__noreturn__));
-void reach_error() { __assert_fail("0", "EvenOdd03WithOverflowBug.c", 3, "reach_error"); }
+void reach_error() { __assert_fail("0", "Addition02WithOverflowBug.c", 3, "reach_error"); }
 
 /*
  * Recursive implementation integer addition.
@@ -12,35 +12,24 @@ void reach_error() { __assert_fail("0", "EvenOdd03WithOverflowBug.c", 3, "reach_
 
 extern int __VERIFIER_nondet_int(void);
 
-int isOdd(int n);
-int isEven(int n);
-
-int isOdd(int n) {
+int addition(int m, int n) {
     if (n == 0) {
-        return 0;
-    } else if (n == 1) {
-        return 1;
-    } else {
-        return isEven(n - 1);
+        return m;
     }
-}
-
-int isEven(int n) {
-    if (n == 0) {
-        return 1;
-    } else if (n == 1) {
-        return 0;
-    } else {
-        return isOdd(n - 1);
+    if (n > 0) {
+        return addition(m+1, n-1);
+    }
+    if (n < 0) {
+        return addition(m-1, n+1);
     }
 }
 
 
 int main() {
+    int m = __VERIFIER_nondet_int();
     int n = __VERIFIER_nondet_int();
-    int result = isEven(n);
-    int mod = n % 2;
-    if (result < 0 || result == mod) {
+    int result = addition(m,n);
+    if (result == m - n) {
         return 0;
     } else {
         ERROR: {reach_error();abort();}
